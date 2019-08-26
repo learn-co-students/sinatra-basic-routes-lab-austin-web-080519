d1fe47dabcd0b4b6de7cd786dad3df1a55df0afb
Cody <lawdermiltcody@gmail.com>
2019-08-26 17:02:35 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        "Hello, World!"
+    end
+    
+    get '/name' do
+    "My name is Cody"
+    end
+
+    get '/hometown' do
+        "My hometown is Philippines"
+    end
+
+
+    get '/favorite-song' do
+        "My favorite song is Nothing"
+    end
+
 end
